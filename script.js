b6bac459ec15817999ada39ba53a9d9c5265f664
donyaesolo <35720283+donyaesolo@users.noreply.github.com>
2019-10-11 23:13:03 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -25,6 +25,7 @@ function turnGreen (elem) {
 function bigBoyTime (elem) {
   elem.style.height = '100%'
 }
+
 function lightsOut (elem) {
   document.body.style.backgroundColor = 'black'
 }
@@ -37,9 +38,12 @@ function boo (elem) {
   document.getElementById('spookie').innerHTML = 'Spookie Mode'
   document.getElementById('spookie').addEventListener('click', spookieModeActivate)
 }
+var skelie = new Boolean (false)
+var n
 function spookieModeActivate () {
+	document.getElementById('title').innerHTML = 'Spookie Mode'
   var background = document.getElementById('spook')
-  var n = 0
+  n = 0
   play1()
   document.getElementById('normal').style.display = 'none'
   document.getElementById('spook').style.display = 'flex'
@@ -65,14 +69,80 @@ function spookieModeActivate () {
       document.getElementById('spook').style.backgroundImage = 'url(images/s.jpg)'
       n = 0
     }
-  }, 1000)
+  }, 769.23)
 }
 
 function play2 () {
+  reset()
+  var background = document.getElementById('spook')
+  n = 6
   document.getElementById('play2').play()
+  document.getElementById('play2').onplaying = function () {
+    document.getElementById('pic1').src = 'images/skelie.gif'
+    document.getElementById('pic2').src = 'images/skelie.gif'
+    background.style.backgroundImage = 'url(images/sS.jpg)'
+    setInterval(function () {
+      if (n === 6) {
+        background.style.backgroundImage = 'url(images/kK.jpg)'
+        n = 7
+      } else if (n === 7) {
+        document.getElementById('spook').style.backgroundImage = 'url(images/e.jpg)'
+        n = 8
+      } else if (n === 8) {
+        document.getElementById('spook').style.backgroundImage = 'url(images/l.jpg)'
+        n = 9
+      } else if (n === 9) {
+        document.getElementById('spook').style.backgroundImage = 'url(images/i.gif)'
+        n = 10
+      } else if (n === 10) {
+        document.getElementById('spook').style.backgroundImage = 'url(images/eE.jpg)'
+        n = 11
+      } else if (n === 11) {
+        document.getElementById('spook').style.backgroundImage = 'url(images/sS.jpg)'
+        n = 6
+      }
+    }, 521.74)
+  }
 }
 function play1 () {
+  reset()
+  var background = document.getElementById('spook')
   document.getElementById('play1').play()
+  background.style.backgroundImage = 'url(images/s.jpg)'
+  document.getElementById('play1').onplaying = function () {
+    document.getElementById('pic1').src = 'images/cutie.gif'
+    document.getElementById('pic2').src = 'images/cutie.gif'
+    setInterval(function () {
+      if (n === 0) {
+        background.style.backgroundImage = 'url(images/p.png)'
+        n = 1
+      } else if (n === 1) {
+        document.getElementById('spook').style.backgroundImage = 'url(images/o.jpg)'
+        n = 2
+      } else if (n === 2) {
+        document.getElementById('spook').style.backgroundImage = 'url(images/oa.jpg)'
+        n = 3
+      } else if (n === 3) {
+        document.getElementById('spook').style.backgroundImage = 'url(images/k.jpg)'
+        n = 4
+      } else if (n === 4) {
+        document.getElementById('spook').style.backgroundImage = 'url(images/y.png)'
+        n = 5
+      } else if (n === 5) {
+        document.getElementById('spook').style.backgroundImage = 'url(images/s.jpg)'
+        n = 0
+      }
+    }, 769.23)
+  }
+}
+
+function reset () {
+  if (skelie === true) {
+    clearInterval()
+    n = 6
+  } else {
+    n = 0
+  }
 }
 // These W3Schools links show efficient ways of completing some of the challenges!
 // https://www.w3schools.com/jsref/prop_style_height.asp
